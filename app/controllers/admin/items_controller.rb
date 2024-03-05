53d3a238fa7bac03ad4e32b4b52859c8c0cd0144
ChiakiKondo <154647391+ckondo9583@users.noreply.github.com>
2024-03-05 14:49:34 +0000

    3/5

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,27 +1,48 @@
 class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
     @items = Item.all
   end
+  
+  def edit
+     @item = Item.find(params[:id])
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      flash[:notice] = "successfully."
+      redirect_to admin_item_path(@item)
+    else
+      render :edit
+    end
+  end
+  
 
   def create
      @item = Item.new(item_params)
   if @item.save
-     redirect_to admin_items_path, notice: '商品が登録されました。'
+     redirect_to admin_item_path(@item), notice: '商品が登録されました。'
   else
      render :new
   end
   end
-  
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def new
     @item = Item.new
   end
+
   
+
   private
 
   def item_params
     params.require(:item).permit(:name, :price,:introduction)
   end
-  
+
 end
